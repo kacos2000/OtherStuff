d0433dde331b8f768f0b6c7ba81c4a5a789cfeb8
Costas K <11378310+kacos2000@users.noreply.github.com>
2019-12-13 12:15:31 +0200

    ExFat Filename Hash Calc

--- ExFat/ExFat_Name_Hash.ps1
+++ ExFat/ExFat_Name_Hash.ps1
@@ -27,7 +27,7 @@ $Hash = 0
 # Start loop from 0 to the length of the filename byte count
 for($index = 0; $index -lt $NumberOfBytes; $index++){
 
-# if previous hash (binary) number ends in 1 add 32768 (0x8000) to the hash value in the next line
+# if previous hash (binary) number ends in 1 (odd numbers) add 32768 (0x8000) to the hash value in the next line
 If ($Hash -band 1) {$h = 0x8000} Else {$h = 0}
 
 # for each iteration add to t$Hash the $h value if any, the binary right shift of the hash value and the filename character integer value
